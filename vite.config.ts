a62f32f76dcde8b34bab5f383ba6210367f8f077
Carl Persson <kalle.r.persson@gmail.com>
2025-04-09 10:59:32 +0200

    updated files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+  plugins: [react()],
+  base: "/", // Ensure correct asset loading in production
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000 KB (1MB)
+  },
+})
